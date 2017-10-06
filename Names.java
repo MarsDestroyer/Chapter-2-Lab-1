045abecb543e71ea0d09bef8ec3f837a5f9fdaf3
ThomasJiang <MarsDestroyer@users.github.com>
2017-10-06 09:24:40 -0700

    APCS-Chapter2-Lab

---
+++ Names.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Names here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Names
+{
+    // instance variables - replace the example below with your own
+    
+    public static void main(String[] args) {
+        System.out.println ("\tStudent\t\t\tHometown\tMajor");
+        System.out.println ("\t=======\t\t\t========\t=====");
+        System.out.println ("\tThomas\t\t\tChina\t\tBiology");
+        System.out.println ("\tColin\t\t\tLafayette\tBiology");
+        System.out.println ("\tCarl\t\t\tLafayette\tComputer Science");
+    }
+}
